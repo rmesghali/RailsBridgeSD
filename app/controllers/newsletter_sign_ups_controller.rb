f8b8e737b90118dc3dd6e235b45d87ac66727c36
kmabrahamson <abrahamson.kaitlin@gmail.com>
2016-06-22 16:08:04 -0700

    one more time

---
+++ app/controllers/newsletter_sign_ups_controller.rb
@@ -0,0 +1,21 @@
+class NewsletterSignUpsController < ApplicationController
+
+  def new
+   @newsletter_sign_up = NewsletterSignUp.new
+  end
+
+  def create
+    @newsletter_sign_up = NewsletterSignUp.new newsletter_sign_up_params
+    if @newsletter_sign_up.save
+      redirect_to root_path, notice: "Submitted Succesfully"
+    else
+      redirect_to root_path, alert: "Submit Unsuccessful"
+    end
+  end
+
+  private
+    def newsletter_sign_up_params
+      params.require(:newsletter_sign_up).permit(:name, :email)
+    end
+
+end
